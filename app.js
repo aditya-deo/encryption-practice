98da0710997bf17e290ee8f9e1ea4ff8689996d6
TECOA136_Aditya_ Deo <ahdeo2015@gmail.com>
2021-09-18 12:38:40 +0530

    add md5

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 
 const app = express();
@@ -20,9 +20,6 @@ const userSchema =new mongoose.Schema( {
     password: String
 })
 
-
-
-userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields:["password"]});
 
 const User = new mongoose.model("user",userSchema);
 
@@ -44,7 +41,7 @@ app.get("/register",function(req,res){
 app.post("/register", function(req,res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     })
     newUser.save(function(err){
         if(err){
@@ -60,7 +57,7 @@ app.post("/register", function(req,res){
 
 app.post("/login", function(req,res){
     const userName = req.body.username;
-    const password = req.body.password;
+    const password = (req.body.password);
 
     User.findOne({email: userName}, function(err, foundUser){
         if(err){
