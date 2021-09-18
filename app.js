d011ae53ac89d7d4d5193534294132f9084f3d75
TECOA136_Aditya_ Deo <ahdeo2015@gmail.com>
2021-09-18 13:46:30 +0530

    try passport.js

--- app.js
+++ app.js
@@ -4,24 +4,45 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5');
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 
 const app = express();
-
-mongoose.connect("mongodb://localhost:27017/userDB");
-
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(session({
+    secret: 'a loooong string',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+  }))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+
 
 const userSchema =new mongoose.Schema( {
     email: String,
     password: String
 })
 
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("user",userSchema);
+
+// CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 
@@ -39,38 +60,13 @@ app.get("/register",function(req,res){
 })
 
 app.post("/register", function(req,res){
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    })
-    newUser.save(function(err){
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.render('secrets');
-        }
-    });
+    
 
 })
 
 
 app.post("/login", function(req,res){
-    const userName = req.body.username;
-    const password = (req.body.password);
-
-    User.findOne({email: userName}, function(err, foundUser){
-        if(err){
-            console.log(err);
-        }
-        else{
-            if(foundUser){
-                if(foundUser.password === password){
-                    res.render('secrets');
-                }
-            }
-        }
-    })
+    
 })
 
 
